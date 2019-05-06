e166fbd57f6552bc3782b04178e544c3073a8969
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-05-06 11:49:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,20 @@
-def reverse_each_word(string)
-  array = string.split("")
-  reversed_string = []
+#def reverse_each_word(string)
+#  array = string.split("")
+#  reversed_string = []
 
-  reversed_string.each do |x|
-    reversed_string.unshift(x)
-  end
+#  reversed_string.each do |x|
+#    reversed_string.unshift(x)
+#  end
 
-  return reversed_string.join('')
+#  return reversed_string.join('')
+#end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
